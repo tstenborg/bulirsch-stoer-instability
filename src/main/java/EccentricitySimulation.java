5d291633dcad8747744ca5359c033dc9979a93dd
tstenborg <50466658+tstenborg@users.noreply.github.com>
2026-09-05 20:48:26 +1000

    Linting.

--- src/main/java/EccentricitySimulation.java
+++ src/main/java/EccentricitySimulation.java
@@ -43,8 +43,7 @@ public final class EccentricitySimulation {
     };
     Table table =
         Table.read().usingOptions(
-            CsvReadOptions.builder(inputStream).columnTypes(types)
-    );
+            CsvReadOptions.builder(inputStream).columnTypes(types));
 
     IntColumn x = table.intColumn("time");
     DoubleColumn yJupCalc = table.doubleColumn("e Jup calc");
@@ -55,41 +54,47 @@ public final class EccentricitySimulation {
     DoubleColumn ySatBS = table.doubleColumn("e Sat Bulirsch-Stoer");
 
     // Configure plot traces.
-    ScatterTrace traceJupExp = ScatterTrace.builder(x, yJupCalc)
-        .mode(ScatterTrace.Mode.LINE)
-        .name("Jupiter, expected")
-        .line(Line.builder().color(HEX_BLACK).dash(Line.Dash.DASH_DOT).build())
-        .build();
+    ScatterTrace traceJupExp =
+        ScatterTrace.builder(x, yJupCalc)
+            .mode(ScatterTrace.Mode.LINE)
+            .name("Jupiter, expected")
+            .line(Line.builder().color(HEX_BLACK).dash(Line.Dash.DASH_DOT).build())
+            .build();
 
-    ScatterTrace traceJupSimRK = ScatterTrace.builder(x, yJupRK)
-        .mode(ScatterTrace.Mode.LINE)
-        .name("Jupiter, simulated")
-        .line(Line.builder().color(HEX_DARK_GREY).build())
-        .build();
+    ScatterTrace traceJupSimRK =
+        ScatterTrace.builder(x, yJupRK)
+            .mode(ScatterTrace.Mode.LINE)
+            .name("Jupiter, simulated")
+            .line(Line.builder().color(HEX_DARK_GREY).build())
+            .build();
 
-    ScatterTrace traceJupSimBS = ScatterTrace.builder(x, yJupBS)
-        .mode(ScatterTrace.Mode.LINE)
-        .name("Jupiter, simulated")
-        .line(Line.builder().color(HEX_DARK_GREY).build())
-        .build();
+    ScatterTrace traceJupSimBS =
+        ScatterTrace.builder(x, yJupBS)
+            .mode(ScatterTrace.Mode.LINE)
+            .name("Jupiter, simulated")
+            .line(Line.builder().color(HEX_DARK_GREY).build())
+            .build();
 
-    ScatterTrace traceSatExp = ScatterTrace.builder(x, ySatCalc)
-        .mode(ScatterTrace.Mode.LINE)
-        .name("Saturn, expected")
-        .line(Line.builder().color(HEX_BLACK).dash(Line.Dash.DOT).build())
-        .build();
+    ScatterTrace traceSatExp =
+        ScatterTrace.builder(x, ySatCalc)
+            .mode(ScatterTrace.Mode.LINE)
+            .name("Saturn, expected")
+            .line(Line.builder().color(HEX_BLACK).dash(Line.Dash.DOT).build())
+            .build();
 
-    ScatterTrace traceSatSimRK = ScatterTrace.builder(x, ySatRK)
-        .mode(ScatterTrace.Mode.LINE)
-        .name("Saturn, simulated")
-        .line(Line.builder().color(HEX_BLUE).build())
-        .build();
+    ScatterTrace traceSatSimRK =
+        ScatterTrace.builder(x, ySatRK)
+            .mode(ScatterTrace.Mode.LINE)
+            .name("Saturn, simulated")
+            .line(Line.builder().color(HEX_BLUE).build())
+            .build();
 
-    ScatterTrace traceSatSimBS = ScatterTrace.builder(x, ySatBS)
-        .mode(ScatterTrace.Mode.LINE)
-        .name("Saturn, simulated")
-        .line(Line.builder().color(HEX_BLUE).build())
-        .build();
+    ScatterTrace traceSatSimBS =
+        ScatterTrace.builder(x, ySatBS)
+            .mode(ScatterTrace.Mode.LINE)
+            .name("Saturn, simulated")
+            .line(Line.builder().color(HEX_BLUE).build())
+            .build();
 
     // Prepare for plotting.
     String plotTitle =
@@ -109,11 +114,12 @@ public final class EccentricitySimulation {
             layoutRK, new Trace[] {traceJupExp, traceSatExp, traceJupSimRK, traceSatSimRK}));
 
     // Plot results of Bulirsch-Stoer simulation.
-    Layout layoutBS = Layout.builder()
-        .title(plotTitle + " (Bulirsch-Stoer)")
-        .xAxis(Axis.builder().title(xAxis).build())
-        .yAxis(Axis.builder().title(yAxis).build())
-        .build();
+    Layout layoutBS =
+        Layout.builder()
+            .title(plotTitle + " (Bulirsch-Stoer)")
+            .xAxis(Axis.builder().title(xAxis).build())
+            .yAxis(Axis.builder().title(yAxis).build())
+            .build();
     Plot.show(
         new Figure(
             layoutBS, new Trace[] {traceJupExp, traceSatExp, traceJupSimBS, traceSatSimBS}));
